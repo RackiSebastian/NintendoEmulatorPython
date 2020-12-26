37000d6c3597ec38598d7ceb63083d923a2b50af
RackiSebastian <118459412@umail.ucc.ie>
2020-12-26 14:56:17 +0000

    Adding some cpu instructions

---
+++ CPU.py
@@ -0,0 +1,11 @@
+class CPU:
+    def __init__(self):
+        self.registers = []
+
+    #Take in instruction as bytes
+    def instr(self,instruction: bytes):
+        #Process instructions
+        print(instruction)
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import argparse
-
+from CPU import CPU
 
 def main():
     parser = argparse.ArgumentParser(description="Nintendo Simulator")
-    parser.add_argument('rom_path',
-                        metavar='R',
-                        type=str,
-                        help='path to nintento rom')
+    parser.add_argument('rom_path')
 
     args = parser.parse_args()
 
     print(args.rom_path)
 
-    '''Reading in ASCII values '''
+    '''Reading in Hex values '''
     with open(args.rom_path,'rb') as SMB_file:
-        print(SMB_file.readlines())
+        l = SMB_file.readlines()
 
-    pass
+
+    cpu = CPU()
+    '''
+    Instructions start at 16kb 
+    Represent each byte of data 
+    8 bits in each byte of data 
+    '''
+    #Change this too hardcoded
+    prg_block = 2
+    instructions = l[0][16:16+16384 * prg_block]
+    print(instructions)
+    print(cpu.instr(l[0][0:3]))
 
 
 if __name__ == '__main__':
     main()
+
